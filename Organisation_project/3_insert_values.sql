b82f7389d27711cc5bbd9c53f230b4b79ba3b295
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-05 20:30:49 +0530

    Update 3_insert_values.sql

--- Organisation_project/3_insert_values.sql
+++ Organisation_project/3_insert_values.sql
@@ -1,1 +1,3 @@
 insert into employee (first_name,last_name,sal,DOJ,phone,department)values ("nitin","Rawat",90000,"2026-11-19","8932178520","IT");
+insert into employee (first_name,last_name,sal,DOJ,department)values ("ankit","singh",85000,"2026-12-19","sales");
+insert into employee (first_name,last_name,sal,DOJ)values ("apurva","vora",45000,"2026-01-19");
