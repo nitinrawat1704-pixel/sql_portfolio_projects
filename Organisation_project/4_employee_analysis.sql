03f464548b743c315f61e0caa1272a1e4ae8df52
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-05 21:03:27 +0530

    Update 4_employee_analysis.sql
    q4 (highest sal)can be solved using 
    select full_name,sal,deparment from employee order by sal desc limit 1;
    but what if there are multiple emp with same highest sal?
    ->this is where limit fails and sub query works.

--- Organisation_project/4_employee_analysis.sql
+++ Organisation_project/4_employee_analysis.sql
@@ -3,5 +3,7 @@ Display all employee records.
 Find employees working in the IT department.
   select * from employee where department ="IT";
 List employees earning more than ₹80,000.
-  select name,sal,department from employee where sal>80000;
+  select full_name,sal,department from employee where sal>80000;
+Find the highest-paid employee.
+  select full_name,sal,department from employee where sal = (select max(sal) from employee);
 
