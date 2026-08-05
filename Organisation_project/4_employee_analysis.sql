7295b79e43176b918c46440eba2313ae17b1fa96
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-05 20:47:07 +0530

    Update 4_employee_analysis.sql

--- Organisation_project/4_employee_analysis.sql
+++ Organisation_project/4_employee_analysis.sql
@@ -1,1 +1,7 @@
+Display all employee records.
+  select * from employee;
+Find employees working in the IT department.
+  select * from employee where department ="IT";
+List employees earning more than ₹80,000.
+  select name,sal,department from employee where sal>80000;
 
