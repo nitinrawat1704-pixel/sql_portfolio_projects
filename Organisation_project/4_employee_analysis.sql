d64761b529a5dee470a0ce407029f682f025db25
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-05 21:04:01 +0530

    Update 4_employee_analysis.sql

--- Organisation_project/4_employee_analysis.sql
+++ Organisation_project/4_employee_analysis.sql
@@ -1,9 +1,9 @@
-Display all employee records.
+Q1.Display all employee records.
   select * from employee;
-Find employees working in the IT department.
+Q2.Find employees working in the IT department.
   select * from employee where department ="IT";
-List employees earning more than ₹80,000.
+Q3.List employees earning more than ₹80,000.
   select full_name,sal,department from employee where sal>80000;
-Find the highest-paid employee.
+Q4.Find the highest-paid employee.
   select full_name,sal,department from employee where sal = (select max(sal) from employee);
 
