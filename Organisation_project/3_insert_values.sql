4babbb6ba3e06b0cf4c8f719c1db20a324d4ddf4
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-06 12:00:41 +0530

    Update 3_insert_values.sql

--- Organisation_project/3_insert_values.sql
+++ Organisation_project/3_insert_values.sql
@@ -1,3 +1,17 @@
 insert into employee (first_name,last_name,sal,DOJ,phone,department)values ("nitin","Rawat",90000,"2026-11-19","8932178520","IT");
 insert into employee (first_name,last_name,sal,DOJ,department)values ("ankit","singh",85000,"2026-12-19","sales");
 insert into employee (first_name,last_name,sal,DOJ)values ("apurva","vora",45000,"2026-01-19");
+INSERT INTO employee (first_name, last_name, sal, DOJ, phone, department)
+VALUES
+('Amit', 'Sharma', 65000, '2025-06-10', '9876543210', 'Sales'),
+('Priya', 'Mehta', 85000, '2024-11-15', '9123456789', 'Finance'),
+('Rahul', 'Verma', 72000, '2023-08-21', '9988776655', 'Admin'),
+('Sneha', 'Patel', 78000, '2026-01-12', '9012345678', 'IT'),
+('Karan', 'Singh', 55000, '2025-03-05', '9090909090', 'Temp'),
+('Anjali', 'Gupta', 120000, '2022-12-01', '9876501234', 'VP'),
+('Rohit', 'Joshi', 68000, '2024-04-18', '8765432109', 'Sales'),
+('Neha', 'Kapoor', 95000, '2023-10-30', '7894561230', 'Finance'),
+('Vikram', 'Rana', 88000, '2026-07-25', '9988123456', 'Admin'),
+('Rakesh', 'Mishra', 75000, '2025-02-14', '9876543210', 'Sales'),
+('Pooja', 'Saxena', 82000, '2026-01-20', '9876543210', 'Finance'),
+('Anand','Nigam',120000,'2026-05-19','8932176620','VP');
