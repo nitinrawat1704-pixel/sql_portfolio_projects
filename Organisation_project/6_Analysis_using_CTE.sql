0f7d5cdd73e892c5ba86a4997a4b523d83325913
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-16 22:52:02 +0530

    Update 6_Analysis_using_CTE.sql

--- Organisation_project/6_Analysis_using_CTE.sql
+++ Organisation_project/6_Analysis_using_CTE.sql
@@ -15,3 +15,19 @@ when diff<0 then "Lower"
 else "Same"
 end as "status"
 from t)
+
+Q2.Management wants to know how much each employee contributes to their department's total salary.
+
+
+WITH T AS
+(
+select 
+full_name,
+department,
+sal,
+sum(sal)over(partition by department) as "department_total_salary"
+from employee
+)
+select *,
+concat(round((sal/department_total_salary)*100,2),"%") as"employee's salary as a percentage of department salary"
+from t;
